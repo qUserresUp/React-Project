6a4096e2b7894955a0b81c82d696cc0754e99af3
Camel <fz326115550@gmail.com>
2019-11-13 07:36:22 -0600

    finished add ingredient function

--- burger-builder/src/component/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/component/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
 
 const buildControls = (props) =>{
     return(
-        <div>
-
+        <div className={styles.BuildControls}>
+            {controls.map((el)=>{
+                return <BuildControl 
+                    key={el.label} 
+                    label={el.label} 
+                    addIngHandle={()=>props.addIngHandle(el.type)}
+                />
+            })}
         </div>
     )
 }
--- burger-builder/src/container/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../component/Burger/Burger';
+import BuildControls from '../../component/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICE = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 
 class BurgerBuilder extends Component{
 
@@ -10,7 +19,20 @@ class BurgerBuilder extends Component{
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4,
+    }
+
+    addIngredientHandler=(type)=>{
+        const newCount = this.state.ingredients[type]+1;
+        let newIng = {...this.state.ingredients};
+        newIng[type] =newCount;
+        let newPrice = this.state.totalPrice+INGREDIENT_PRICE[type];
+
+        this.setState({
+            ingredients: newIng, 
+            totalPrice: newPrice
+        });
     }
 
     render(){
@@ -18,7 +40,7 @@ class BurgerBuilder extends Component{
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger Controls</div>
+                <BuildControls addIngHandle={this.addIngredientHandler}/>
             </Aux>
         )
     }
